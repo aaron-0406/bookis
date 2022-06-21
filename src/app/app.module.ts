3501e3eb3c1eba72aa7c67d8391905bfabcc23fd
Angie Cabanillas <acabanillasb2@upao.edu.pe>
2022-06-21 16:19:09 -0500

    chore: Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateBookComponent } from './Book/create-book/create-book.component';
+import { EditBookComponent } from './Components/Book/edit-book/edit-book.component';
+import { ListBookComponent } from './Components/Book/list-book/list-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateBookComponent,
+    EditBookComponent,
+    ListBookComponent
   ],
   imports: [
     BrowserModule,
