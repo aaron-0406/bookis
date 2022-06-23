2486884e6ffcecdabc5b3e2297d7ab274731c63f
Angie Cabanillas <acabanillasb2@upao.edu.pe>
2022-06-23 18:37:03 -0500

    chore: Add array of books

--- src/app/Components/Book/list-book/list-book.component.ts
+++ src/app/Components/Book/list-book/list-book.component.ts
@@ -1,20 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BookService } from 'src/app/Services/book.service';
 
+export interface Books {
+  titulo: string;
+  autor: string;
+  genero: string;
+  editorial: string;
+  precio: number;
+  sinopsis: string;
+  imagen:string,
+}
 @Component({
   selector: 'app-list-book',
   templateUrl: './list-book.component.html',
-  styleUrls: ['./list-book.component.css']
+  styleUrls: ['./list-book.component.css'],
 })
 export class ListBookComponent implements OnInit {
+  listBooks: Books[] = [
+    {
+      titulo: 'Insomnio',
+      autor: 'autor1',
+      genero: 'dramatico',
+      editorial: 'editorial lo que sea',
+      precio: 125,
+      sinopsis: 'Esta es una breve descripcion acerca del libro bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
+      imagen:'assets/images/insomnio.jpg',
+    },
+  ];
 
-  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  originally bred for hunting.`;
-
-  constructor(private router: Router) { }
+  constructor(private _bookService:BookService) {}
 
   ngOnInit(): void {
+    this.cargarBooks();
   }
 
+  bookMostrar:Books = this._bookService.listBooks[0];
+
+  cargarBooks() {
+    this.listBooks = this._bookService.getBooks();
+  }
 }
--- src/app/Services/book.service.ts
+++ src/app/Services/book.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Books } from '../Components/Book/list-book/list-book.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
+  listBooks:Books[] = [
+    {
+      titulo: 'Insomnio',
+      autor: 'autor1',
+      genero: 'dramatico',
+      editorial: 'editorial lo que sea',
+      precio: 125,
+      sinopsis: 'Esta es una breve descripcion acerca del libro bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
+      imagen:'assets/images/insomnio.jpg',
+    },
+    {
+      titulo: 'Insomnio',
+      autor: 'autor1',
+      genero: 'dramatico',
+      editorial: 'editorial lo que sea',
+      precio: 125,
+      sinopsis: 'Esta es una breve descripcion acerca del libro bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
+      imagen:'assets/images/insomnio.jpg',
+    },
+    {
+      titulo: 'Insomnio',
+      autor: 'autor1',
+      genero: 'dramatico',
+      editorial: 'editorial lo que sea',
+      precio: 125,
+      sinopsis: 'Esta es una breve descripcion acerca del libro bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
+      imagen:'assets/images/insomnio.jpg',
+    }
+  ]
+
   constructor() { }
+
+  getBooks() {
+    return this.listBooks.slice();
+  }
 }
