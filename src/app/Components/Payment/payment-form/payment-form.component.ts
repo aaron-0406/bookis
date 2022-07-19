0d401555724c38711ccef07b017633b662f81d76
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-07-18 21:58:16 -0500

    chore: add pagos con paypal

--- src/app/Components/Payment/payment-form/payment-form.component.ts
+++ src/app/Components/Payment/payment-form/payment-form.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { render } from 'creditcardpayments/creditCardPayments';
 
 @Component({
   selector: 'app-payment-form',
   templateUrl: './payment-form.component.html',
-  styleUrls: ['./payment-form.component.css']
+  styleUrls: ['./payment-form.component.css'],
 })
 export class PaymentFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    render({
+      id: '#myPaypalButtons',
+      currency: 'PE',
+      value: '100.00',
+      onApprove: (details) => {
+        alert('Transaction Successfull');
+      },
+    });
   }
 
+  ngOnInit(): void {}
 }
